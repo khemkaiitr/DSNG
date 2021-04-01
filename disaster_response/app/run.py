dfecead4be5c081fa32f34c2b6d263e7c23f9ed4
saurabh <saurabh.khemka@walmart.com>
2021-04-01 11:26:44 +0530

    feat: added another visualization to display representation of different categories.

--- disaster_response/app/run.py
+++ disaster_response/app/run.py
@@ -13,8 +13,8 @@ from plotly.graph_objs import Bar
 # from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -26,6 +26,7 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -40,39 +41,43 @@ model = pickle.load(open("../models/classifier.pkl", 'rb'))
 @app.route('/')
 @app.route('/index')
 def index():
-    
+    graphs = []
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
 
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
-    
+    graph_one = [Bar(
+        x=genre_names,
+        y=genre_counts)]
+
+    layout_one = dict(
+        title='Distribution of Message Genres',
+        y_axis=dict(title="Count"),
+        x_axis=dict(title='Genre')
+    )
+
+    # Graph 2 to display distribution of different categories
+    categories = df.iloc[:, 4:]
+    categories_sum = categories.sum(axis=0).sort_values(ascending=False)
+    categories_names = list(categories_sum.index)
+
+    graph_two = [Bar(
+        x=categories_names,
+        y=categories_sum)]
+
+    layout_two = dict(
+        title='Distribution of different categories',
+        y_axis=dict(title="Count"),
+        x_axis=dict(title='Category')
+    )
+
+    graphs.append(dict(data=graph_one, layout=layout_one))
+    graphs.append(dict(data=graph_two, layout=layout_two))
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -81,7 +86,7 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
@@ -96,10 +101,9 @@ def go():
 
 
 def main():
-#     app.run(host='0.0.0.0', port=3001, debug=True)
+    #     app.run(host='0.0.0.0', port=3001, debug=True)
     app.run(port=8080)
 
 
-    
 if __name__ == '__main__':
     main()
